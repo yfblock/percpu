d5a3c7eb48af3a4bdd330f37445d79678d453f38
Yuekai Jia <equation618@gmail.com>
2025-01-19 00:03:50 +0800

    Detect number of CPUs on `init`

--- percpu/src/imp.rs
+++ percpu/src/imp.rs
@@ -10,12 +10,20 @@ const fn align_up_64(val: usize) -> usize {
 #[cfg(not(target_os = "none"))]
 static PERCPU_AREA_BASE: spin::once::Once<usize> = spin::once::Once::new();
 
+extern "C" {
+    fn _percpu_start();
+    fn _percpu_end();
+    fn _percpu_load_start();
+    fn _percpu_load_end();
+}
+
+/// Returns the number of per-CPU data areas reserved.
+pub fn percpu_area_num() -> usize {
+    (_percpu_end as usize - _percpu_start as usize) / align_up_64(percpu_area_size())
+}
+
 /// Returns the per-CPU data area size for one CPU.
 pub fn percpu_area_size() -> usize {
-    extern "C" {
-        fn _percpu_load_start();
-        fn _percpu_load_end();
-    }
     // It seems that `_percpu_load_start as usize - _percpu_load_end as usize` will result in more instructions.
     use percpu_macros::percpu_symbol_offset;
     percpu_symbol_offset!(_percpu_load_end) - percpu_symbol_offset!(_percpu_load_start)
@@ -27,9 +35,6 @@ pub fn percpu_area_size() -> usize {
 pub fn percpu_area_base(cpu_id: usize) -> usize {
     cfg_if::cfg_if! {
         if #[cfg(target_os = "none")] {
-            extern "C" {
-                fn _percpu_start();
-            }
             let base = _percpu_start as usize;
         } else {
             let base = *PERCPU_AREA_BASE.get().unwrap();
@@ -38,41 +43,46 @@ pub fn percpu_area_base(cpu_id: usize) -> usize {
     base + cpu_id * align_up_64(percpu_area_size())
 }
 
-/// Initialize the per-CPU data area for `max_cpu_num` CPUs.
-pub fn init(max_cpu_num: usize) {
+/// Initialize all per-CPU data areas.
+///
+/// The number of areas is determined by the following formula:
+///
+/// ```text
+/// (percpu_section_size / align_up(percpu_area_size, 64)
+/// ```
+///
+/// Returns the number of areas initialized. If this function has been called
+/// before, it does nothing and returns 0.
+pub fn init() -> usize {
     // avoid re-initialization.
     if IS_INIT
         .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
         .is_err()
     {
-        return;
+        return 0;
     }
-
-    let size = percpu_area_size();
 
     #[cfg(target_os = "linux")]
     {
         // we not load the percpu section in ELF, allocate them here.
-        let total_size = align_up_64(size) * max_cpu_num;
+        let total_size = _percpu_end as usize - _percpu_start as usize;
         let layout = std::alloc::Layout::from_size_align(total_size, 0x1000).unwrap();
         PERCPU_AREA_BASE.call_once(|| unsafe { std::alloc::alloc(layout) as usize });
     }
 
     let base = percpu_area_base(0);
-    for i in 1..max_cpu_num {
+    let size = percpu_area_size();
+    let num = percpu_area_num();
+    for i in 1..num {
         let secondary_base = percpu_area_base(i);
         #[cfg(target_os = "none")]
-        {
-            extern "C" {
-                fn _percpu_end();
-            }
-            assert!(secondary_base + size <= _percpu_end as usize);
-        }
+        assert!(secondary_base + size <= _percpu_end as usize);
         // copy per-cpu data of the primary CPU to other CPUs.
         unsafe {
             core::ptr::copy_nonoverlapping(base as *const u8, secondary_base as *mut u8, size);
         }
     }
+    num
 }
 
 /// Reads the architecture-specific per-CPU data register.
